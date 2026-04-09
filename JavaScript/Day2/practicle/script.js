551bf9dedc933ffd9729f730d6c40a7e9c9cb8e4
AryanSrivastavaGit <aryansrivastavag00916@gmail.com>
2026-04-09 12:47:30 +0530

    day2

---
+++ JavaScript/Day2/practicle/script.js
@@ -0,0 +1,57 @@
+// // let num = 5;
+
+// let num = Number(prompt("Enter num: "));
+// // num = parseInt(num);
+// console.log(typeof num);
+// if(num<0){
+//     console.log("smaller than 0");
+// }else if(num==0){
+//     console.log("equal than 0");
+// }else{
+//     console.log("greater than 0");
+// }
+
+
+
+// let num = Number(prompt("Enter num: "));
+
+// if(num<8){
+//     for(let i=0; i<num; i++){
+//         console.log("it is "+ num);
+//     }
+// }else{
+//     for(let i=0; i<num*num; i++){
+//         console.log("it is " + num);
+//     }
+// }
+
+
+
+
+function display(){
+    console.log("Hello");
+}
+
+display();
+
+function display(name){
+    console.log("Hello " + name);
+}
+
+display();
+
+// let greet = function(name){
+//     console.log("Hello " + name);
+
+// }
+
+// greet();
+
+
+let greet = function display1(name){
+    console.log("Hello " + name);
+}
+
+greet();
+
+// display1(); 
